6b8bf4b2f220b5746200785b84175029a14142cc
subhamyadav580 <subhamyadav580@gmail.com>
2022-01-14 19:01:12 +0530

    Adding mysql database

--- youtube/youtube/settings.py
+++ youtube/youtube/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'youtube.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE'  : 'django.db.backends.mysql',  
+        'NAME'    : 'quizdatabase',                  
+        'USER'    : 'admin',                     
+        'PASSWORD': 'Admin716381',              
+        'HOST'    : 'localhost',                
+        'PORT'    : '3306',
     }
 }
 
